b011f3d7b96d88be613a017887a294b8ad1babd1
Chris Culpepper <cculpepper1214@gmail.com>
2022-01-15 16:08:47 -0500

    A commit long in coming, life and stuff
    Now the sunrise, moonrise and moon phases are calculated and updated in the sun.h file

--- sunriseApp/gen_sun.py
+++ sunriseApp/gen_sun.py
@@ -1,4 +1,8 @@
-import astral
+#  import astral
+from astral import LocationInfo
+from astral.sun import sun
+#  from astral import moon
+
 import os
 import math
 import datetime
@@ -6,8 +10,18 @@ import pytz
 import moon
 import ephem
 
-city_name = "Hartford"
 
+city_name = "Hartford"
+lat = 41.821118205458426
+lon = -72.62688263153609
+city = LocationInfo(city_name, "USA", "US/Eastern", lat, lon)
+# The only important thing is the lat/lon, the rest are user fillable?
+ephemCity = ephem.Observer()
+ephemCity.lat = str(lat)
+ephemCity.lon = str(lon)
+
+
+#Output files
 fname = "sun.h"
 f = open(fname, 'w')
 headerText = """
@@ -15,24 +29,28 @@ headerText = """
 headerText  = headerText + ".\n\n\n"
 f.write(headerText)
 
+earliestSunsetHour = 16
+earliestSunsetMinute = 00
 
 
-a = astral.Astral()
 
-a.solar_depression = "civil"
+#  a = astral.Astral()
+#  s = sun(city.observer,
 
-city = a[city_name]
-#import pdb;pdb.set_trace()
-latitude = str(city.latitude)
-longitude = str(city.longitude)
+#  a.solar_depression = "civil"
 
-ephemCity = ephem.Observer()
-ephemCity.lat, ephemCity.lon = latitude, longitude
+#  city = a[city_name]
+#  #import pdb;pdb.set_trace()
+#  latitude = str(city.latitude)
+#  longitude = str(city.longitude)
+
+#  ephemCity = ephem.Observer()
+#  ephemCity.lat, ephemCity.lon = latitude, longitude
 #ephemCity.pressure = 0
 #ephemCity.horizon = '-0.34'
 
 
-#Going to generate a list. We are going to calculate the date in th form if day of th e year. 
+#Going to generate a list. We are going to calculate the date in the form of day of the year. 
 #To do this we will add each of the number of days of the month for whichever month that we are in. 
 #We will also account for the leap years directly. Which means we will need to rewrite this every so often. 
 
@@ -40,11 +58,11 @@ ephemCity.lat, ephemCity.lon = latitude, longitude
 #Sunrise will be the number of minutes after 0500 each day. 
 
 
-NYC_TIME = pytz.timezone('America/New_York')
+localTimezone = pytz.timezone('America/New_York')
 
 numDays = [31,28,31,30,31,30,31,31,30,31,30,31]
-leapYears = [2016,2020,2024,2028,2032]
-firstYear = 2021
+leapYears = [2024,2028,2032]
+firstYear = 2022
 numYears = 7
 # Lets print out the number of days in each month and what the leap years to
 # the file, as well as the starting year
@@ -56,7 +74,7 @@ sunriseStr = "// The sunrise times are noted in minutes past 0500 for that day. 
 sunriseStr = sunriseStr + "// These times take into account daylight savings time. \n "
 sunriseStr = sunriseStr + "static const uint8_t sunrise_times[] = {"
 
-sunsetStr = "// The sunset times are noted in minutes past 1645 for that day. \n"
+sunsetStr = "// The sunset times are noted in minutes past " + str(earliestSunsetHour) + str(earliestSunsetMinute) + " for that day. \n"
 sunsetStr = sunsetStr + "// These times take into account daylight savings time. \n "
 sunsetStr = sunsetStr + "static const uint8_t sunset_times[] = {"
 
@@ -76,9 +94,10 @@ previousMoonPhase = 0
 
 #Set up the first moonset data that we save
 ephemCity.date = ephem.Date(datetime.datetime(firstYear, 1,1))
+firstDate = datetime.date(firstYear, 1,1)
 #print(ephemCity.date)
-moon = ephem.Moon(ephemCity)
-previousMoonset = ephemCity.next_setting(moon)
+ephemCity.date = firstDate
+previousMoonset = ephemCity.next_setting(ephem.Moon())
 previousMoonset = ephem.localtime(previousMoonset)
 previousSixMinutes = (previousMoonset.time().hour * 10) + (previousMoonset.time().minute / 6)
 previousSixMinutes = math.ceil(previousSixMinutes)
@@ -95,16 +114,18 @@ for year in range(firstYear, firstYear + numYears):
             dayNum = dayNum + 1;
             date = datetime.date(year, month + 1, day + 1)
             dateTime = datetime.datetime(year, month + 1, day +1)
-            #print(str(date))
-            sun = city.sun(date, local=True)
+            #  print(str(date))
+            #  sun = city.sun(date, local=True)
             
 
             ephemCity.date = ephem.Date(date)
-            #print(ephemCity.date)
+            #  import pdb;pdb.set_trace()
+
+            #  print(ephemCity.date)
             moon = ephem.Moon(ephemCity)
             ephemSun = ephem.Sun(ephemCity)
             moonPhase = moon.phase
-            #import pdb;pdb.set_trace()
+
             if moonPhase > previousMoonPhase:
                 moonPhase = moonPhase + 101
             moonPhase = round(moonPhase)
@@ -112,20 +133,20 @@ for year in range(firstYear, firstYear + numYears):
             previousMoonPhase = moon.phase
 
             moonrise = ephemCity.previous_rising(moon)
+            #  print(moonrise)
             moonrise = ephem.localtime(moonrise)
+            #  print(moonrise)
 
-            #minutes = (delta.total_seconds()/(60*6))
             sixMinutes = (moonrise.time().hour * 10) + (moonrise.time().minute / 6)
             sixMinutes = math.ceil(sixMinutes)
-            #print("moonrise in tenths: " + str(math.ceil(sixMinutes)))
-            #print("moonrise: " + str(moonrise))
+            #  print("moonrise in tenths: " + str(math.ceil(sixMinutes)))
+            #  print("moonrise: " + str(moonrise))
             riseHours = math.floor(sixMinutes / 10)
             riseMinutes = (sixMinutes % 10 ) * 6
-            #print("Calced rise: " + str(riseHours) + ":" + str(riseMinutes))
+            #  print("Calced rise: " + str(riseHours) + ":" + str(riseMinutes))
             #import pdb;pdb.set_trace()
-            #print("SunriseA: " + str(ephemCity.next_setting(ephemSun)))
             moonriseStr = moonriseStr + str(sixMinutes) + ", "
-            #print("        dayNum " + str(dayNum) + " moonrise minutes " + str(sixMinutes) + " calced rise " + str(riseHours) + ":" + str(riseMinutes) +  " time " + str(moonrise))
+            print("        dayNum " + str(dayNum) + "|| calced rise " + str(riseHours) + ":" + str(riseMinutes) +  " || Real rise time " + str(moonrise))
 
             moonset = ephemCity.next_setting(moon, start=date)
             moonset = ephem.localtime(moonset)
@@ -139,7 +160,7 @@ for year in range(firstYear, firstYear + numYears):
             # We need to be fancy with the moonset. We need to save the previous days 
             # moonrise time, and use that if the resultant calculated moonrise is 
             # more than 1 day in the future. 
-            
+
             if ((moonset.date() - moonrise.date()).total_seconds()) > 13*60*60:
                 #Check if the moonset is later than the maximum amount of time
                 # the moon can be in the sky, 12.5 hours.
@@ -148,11 +169,11 @@ for year in range(firstYear, firstYear + numYears):
                 moonset, previousMoonset = previousMoonset, moonset
                 setHours = math.floor(sixMinutes / 10)
                 setMinutes = (sixMinutes % 10 ) * 6
-                #print("swapped dayNum " + str(dayNum) + " moonset minutes " + str(sixMinutes) + " calced rise " + str(setHours) + ":" + str(setMinutes) +  " time " + str(moonset))
+                #  print("swapped dayNum " + str(dayNum) + " moonset minutes " + str(sixMinutes) + " calced rise " + str(setHours) + ":" + str(setMinutes) +  " time " + str(moonset))
             else:
                 setHours = math.floor(sixMinutes / 10)
                 setMinutes = (sixMinutes % 10 ) * 6
-                #print("noswap  dayNum " + str(dayNum) + " moonset minutes " + str(sixMinutes) + " calced rise " + str(setHours) + ":" + str(setMinutes) +  " time " + str(moonset))
+                #  print("noswap  dayNum " + str(dayNum) + " moonset minutes " + str(sixMinutes) + " calced rise " + str(setHours) + ":" + str(setMinutes) +  " time " + str(moonset))
 
             #print("Calced set: " + str(setHours) + ":" + str(setMinutes))
             #import pdb;pdb.set_trace()
@@ -164,27 +185,32 @@ for year in range(firstYear, firstYear + numYears):
 
 
 
-            sunrise = sun["sunrise"]
-            earlyTime = datetime.time(5,00, tzinfo = NYC_TIME)
+            s = sun(city.observer, date=date, tzinfo = localTimezone)
+            sunrise = s["sunrise"]
+            earlyTime = datetime.time(5,00, tzinfo = localTimezone)
             earliestTime = datetime.datetime.combine(date, earlyTime) - sunrise.dst()
             delta = sunrise - earliestTime
             minutes = (delta.total_seconds()/60) - 5 #Needed to add a minus 5 because I have no idea. 
             minutes = math.ceil(minutes)
-            #print("Sunrise: " + str(math.ceil(minutes)))
-            #print("Sunrise: " + str(sunrise))
+            #  import pdb;pdb.set_trace()
+            #  print("Sunrise: " + str(math.ceil(minutes)))
+            #  print("Sunrise: " + str(sunrise))
             sunriseStr = sunriseStr + str(minutes) + ", "
 
-            sunset = sun["sunset"]
-            earlyTime = datetime.time(16,45, tzinfo = NYC_TIME)
+            sunset = s["sunset"]
+            #  sunset = localTimezone.localize(sunset)
+            earlyTime = datetime.time(earliestSunsetHour,earliestSunsetMinute, tzinfo = localTimezone)
             earliestTime = datetime.datetime.combine(date, earlyTime) - sunset.dst()
             delta = sunset - earliestTime
             minutes = (delta.total_seconds()/60) -5 #Needed to add a minus 5 because I have no idea. 
             minutes = math.ceil(minutes)
             if (minutes <0):
                 minutes = 0;
-            #print("Sunset: " + str(math.ceil(minutes)))
-            #print("Sunset: " + str(sunset))
+            #  print("Sunset: " + str(math.ceil(minutes)))
+            #  print("Sunset: " + str(sunset))
             sunsetStr = sunsetStr + str(minutes) + ", "
+
+#Finish writing out the header
 sunriseStr = sunriseStr[:-2]
 f.write(sunriseStr + "};\n")
 
@@ -200,6 +226,9 @@ f.write(numDaysStr)
 f.write(leapYearsStr)
 f.write("uint16_t first_year = " + str(firstYear) + ";\n")
 f.write("uint8_t num_years = " + str(numYears) + ";\n")
+
+f.write("uint8_t earliestSunSetHour = " + str(earliestSunsetHour) + ";\n")
+f.write("uint8_t earliestSunSetMinute = " + str(earliestSunsetMinute) + ";\n")
 moonIllumFracStr = moonIllumFracStr[:-2]
 #f.write(moonIllumFracStr + "};\n")
 
